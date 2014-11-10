14093cd3f68c92e2aa0da2c1bd08650391d2a666
aghatpande <aghatpande@gmail.com>
2014-11-10 03:05:07 -0500

    script to calculate means of pollutants across the specified set of monitors for the pollution monitor data

---
+++ pollutantmean.R
@@ -0,0 +1,6 @@
+pollutantmean <- function(directory,pollutant,id=1:332){
+  ## 'directory' is a character vector of length 1 indicating the location of the csv files
+  ## 'pollutant' is a character vector of length 1 indicating the name of the pollutant for which we will calculate the mean; either "sulfate" or "nitrate"
+  ## 'id' is an integer vector indicating the monitor numbers to be used
+  ## Return the mean of the pollutant across all monitors listed in the 'id' vector (ignoring NA values)
+}
