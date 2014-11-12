9b5e34d34b989713337aa0bcd7b49d57cce05e56
aghatpande <aghatpande@gmail.com>
2014-11-12 04:36:51 -0500

    working script for part 1-4 of assignment 1 of R prog class

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,4 +1,4 @@
-pollutantmean <- function(directory,pollutant,id){
+pollutantmean <- function(directory,pollutant,id=1:332){
   ## 'directory' is a character vector of length 1 indicating the location of the csv files
   ## 'pollutant' is a character vector of length 1 indicating the name of the pollutant for which we will calculate the mean; either "sulfate" or "nitrate"
   ## 'id' is an integer vector indicating the monitor numbers to be used
